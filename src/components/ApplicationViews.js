54fe62a61c4bef924dcc10afd3a8e365d5c50ed6
THATBLADEBOY <aus10b@gmail.com>
2019-01-21 10:24:22 -0600

    completed practices through CH5

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -7,47 +7,70 @@ import OwnerList from './owner/OwnerList'
 
 
 class ApplicationViews extends Component {
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
-
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
-
-    animalsFromAPI = [
-        { id: 1, name: "Doodles", species: "Cat" },
-        { id: 2, name: "Jack", species: "Rabbit" },
-        { id: 3, name: "Angus", species: "Turtle" },
-        { id: 4, name: "Henley", species: "Scorpion" },
-        { id: 5, name: "Derkins", species: "Dog" },
-        { id: 6, name: "Checkers", species: "Koala" }
-    ]
-
-    ownersFromAPI = [
-        { id: 1, name: "Hank Hill", phoneNumber: "931-500-9283" },
-        { id: 2, name: "Finn the Human", phoneNumber: "123-453-6354" },
-        { id: 3, name: "Steven Universe", phoneNumber: "949-828-8437" },
-        { id: 4, name: "Bojack Horseman", phoneNumber: "718-382-1342" }
-    ]
-
-
-// If you haven't created the owners array in your state yet, please go ahead and create it now, and populate it with 4 owners. Each owners should have the id, phoneNumber, and name properties.
-
-// Create a link in your navigation bar that links to /owners path.
-// Create a route for /owners that renders the <OwnerList> component and sends the corresponding state property.
-// Add the code in <OwnerList> to display all the items in the array.
 
     state = {
-        employees: this.employeesFromAPI,
-        locations: this.locationsFromAPI,
-        animals: this.animalsFromAPI,
-        owners: this.ownersFromAPI
+        employees: [],
+        locations: [],
+        animals: [],
+        owners: []
     }
+
+    componentDidMount() {
+        const newState = {}
+    
+        fetch("http://localhost:5002/animals")
+            .then(r => r.json())
+            .then(animals => newState.animals = animals)
+            .then(() => fetch("http://localhost:5002/employees")
+            .then(r => r.json()))
+            .then(employees => newState.employees = employees)
+            .then(() => fetch("http://localhost:5002/locations")
+            .then(r => r.json()))
+            .then(locations => newState.locations = locations)
+            .then(() => fetch("http://localhost:5002/owners")
+            .then(r => r.json()))
+            .then(owners => newState.owners = owners)
+            .then(() => this.setState(newState))
+    }
+
+        deleteAnimal = id => {
+            return fetch(`http://localhost:5002/animals/${id}`, {
+                method: "DELETE"
+            })
+            .then(e => e.json())
+            .then(() => fetch(`http://localhost:5002/animals`))
+            .then(e => e.json())
+            .then(animals => this.setState({
+                animals: animals
+            })
+            )
+        }
+
+        deleteEmployee = id => {
+            return fetch(`http://localhost:5002/employees/${id}`, {
+                method: "DELETE"
+            })
+            .then(r => r.json())
+            .then(() => fetch(`http://localhost:5002/employees`))
+            .then(r => r.json())
+            .then(employees => this.setState({
+                employees: employees
+            }))
+        }
+
+        deleteOwner = id => {
+            return fetch(`http://localhost:5002/owners/${id}`, {
+                method: "DELETE"
+            })
+            .then(r => r.json())
+            .then(() => fetch(`http://localhost:5002/ownerss`))
+            .then(r => r.json())
+            .then(owners => this.setState({
+                owners: owners
+            }))
+        }
+
+
 
     render() {
         return (
@@ -55,14 +78,14 @@ class ApplicationViews extends Component {
                 <Route exact path="/" render={(props) => {
                     return <LocationList locations={this.state.locations} />
                 }} />
-                <Route path="/animals" render={(props) => {
-                    return <AnimalList animals={this.state.animals} />
+                <Route exact path="/animals" render={(props) => {
+                    return <AnimalList deleteAnimal={this.deleteAnimal} animals={this.state.animals} />
                 }} />
                 <Route path="/employees" render={(props) => {
-                    return <EmployeeList employees={this.state.employees} />
+                    return <EmployeeList deleteEmployee={this.deleteEmployee} employees={this.state.employees} />
                 }} />
                 <Route path="/owners" render={(props) => {
-                    return <OwnerList owners={this.state.owners} />
+                    return <OwnerList deleteOwners={this.deleteOwner} owners={this.state.owners} />
                 }} />
             </React.Fragment>
         )
--- src/components/animals/AnimalList.js
+++ src/components/animals/AnimalList.js
@@ -1,47 +1,77 @@
 import React, { Component } from 'react'
+import dog from "./DogIcon.png"
+import "./Animal.css"
 
-
-export default class AnimalList  extends Component {
-
-    
-
-    
-
-    render() {
-
-        // let theOwners = this.props.owners
-        // let theAnimals = this.props.animals
-
-        //  theAnimals.forEach(animal => {
-        //     theOwners.forEach(owner => {
-        //         if (owner.ownerId === animal.ownerId) {
-        //             console.log(owner, animal)
-        //         }
-        //     })
-        // })
-
-        // let ownersOfAnimals = theOwners.forEach(owner => {
-        //     console.log(owner.ownerId);
-        // })
-
-        // console.log(theOwners, theAnimals);
-
-        // let ownerOfAnimal = theOwners.forEach(owner => this.props.owners.find(owner => owner.id === this.props.animals.ownerId))
-
-        // console.log(ownerOfAnimal);
-
+export default class AnimalList extends Component {
+    render () {
         return (
-            <article className="animals">
-            <h3>Animals</h3>
+            <section className="animals">
             {
                 this.props.animals.map(animal =>
-                    <div key={animal.id}>
-                        {animal.name} the {animal.species}
-                        
+                    <div key={animal.id} className="card">
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                <img src={dog} className="icon--dog" />
+                                {animal.name}
+                                <a href="#"
+                                    onClick={() => this.props.deleteAnimal(animal.id)}
+                                    className="card-link">Delete</a>
+                            </h5>
+                        </div>
                     </div>
-                    )
+                )
             }
-            </article>
-        );
+            </section>
+        )
     }
 }
+
+
+
+// import React, { Component } from 'react'
+
+
+// export default class AnimalList  extends Component {
+
+    
+
+    
+
+//     render() {
+
+//         // let theOwners = this.props.owners
+//         // let theAnimals = this.props.animals
+
+//         //  theAnimals.forEach(animal => {
+//         //     theOwners.forEach(owner => {
+//         //         if (owner.ownerId === animal.ownerId) {
+//         //             console.log(owner, animal)
+//         //         }
+//         //     })
+//         // })
+
+//         // let ownersOfAnimals = theOwners.forEach(owner => {
+//         //     console.log(owner.ownerId);
+//         // })
+
+//         // console.log(theOwners, theAnimals);
+
+//         // let ownerOfAnimal = theOwners.forEach(owner => this.props.owners.find(owner => owner.id === this.props.animals.ownerId))
+
+//         // console.log(ownerOfAnimal);
+
+//         return (
+//             <article className="animals">
+//             <h3>Animals</h3>
+//             {
+//                 this.props.animals.map(animal =>
+//                     <div key={animal.id}>
+//                         {animal.name} the {animal.species}
+                        
+//                     </div>
+//                     )
+//             }
+//             </article>
+//         );
+//     }
+// }
